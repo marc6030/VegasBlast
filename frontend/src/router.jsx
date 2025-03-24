75da23e1a13e041075479c43eb502198d78bdf83
PhillipNguyenCong <144035124+PhillipNguyenCong@users.noreply.github.com>
2025-03-24 13:31:38 +0100

    ok

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import MineBlast from "./pages/MineBlast";
-import MineBlastLightning from "./pages/MineBlastLightning";
+import MineBlastlightning from "./pages/MineBlastlightning";
 import Login from "./pages/LoginPage";
 import Signup from "./pages/SignupPage";
 import SaldoPage from "./pages/SaldoPage";
@@ -8,8 +8,8 @@ import SaldoPage from "./pages/SaldoPage";
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/mineblast" element={<MineBlast />} />
-      <Route path="/mineblastlightning" element={<MineBlastLightning />} />
+      <Route path="/MineBlast" element={<MineBlast />} />
+      <Route path="/MineBlastlightning" element={<MineBlastlightning />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/change-saldo" element={<SaldoPage />} />
