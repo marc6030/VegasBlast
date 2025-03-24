cf44dccbc40b42d48f80521a0b42d6305a04ae77
PhillipNguyenCong <144035124+PhillipNguyenCong@users.noreply.github.com>
2025-03-24 15:33:30 +0100

    animation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,8 +12,6 @@ function Navbar() {
                 <h1>VegasBlast 🎰</h1>
             </Link>
             <ul className="nav-links">
-                <li><Link to="/MineBlast">MineBlast</Link></li>
-                <li><Link to="/MineBlastlightning">MineBlast Lightning</Link></li>
                 {user ? (
                     // 📌 Hvis brugeren er logget ind, vis brugernavn, saldo + log ud-knap
                     <>
