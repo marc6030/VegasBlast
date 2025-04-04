c6f6b06468552375db85087e370bef243b618bac
PhillipNguyenCong <144035124+PhillipNguyenCong@users.noreply.github.com>
2025-04-04 13:14:09 +0200

    ok

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,21 @@
 require('dotenv').config();  // Hent miljøvariabler fra .env
 const mysql = require('mysql2/promise');
 
+const CryptoJS = require('crypto-js');
+
+const secret = process.env.secretKey;
+
+const encryptedPassword = process.env.encryptedPassword;
+
+const decrypted = CryptoJS.AES.decrypt(encryptedPassword, secret);
+
+const dbPassword = decrypted.toString(CryptoJS.enc.Utf8);
+
 const getConnection = async () => {
     return await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        password: dbPassword,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT
     });
